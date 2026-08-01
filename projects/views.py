72efdec630a2e852b0f106446ad47eebcb21ce5d
etienne <etienne@email.com>
2026-08-01 14:24:50 +0000

    feat: create project viewset

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    API CRUD des projets.
+    """
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
