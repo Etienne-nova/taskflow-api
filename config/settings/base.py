51d7005e238caaea58f20c4b45c091c09ab374aa
etienne <etienne@email.com>
2026-08-01 14:15:47 +0000

    feat: integrate Django REST Framework

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "accounts",
     "projects",
     "tasks",   
@@ -95,6 +96,14 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
