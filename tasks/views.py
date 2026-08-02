59af8207d9ad96f6af40df379caac14e1f407fd4
etienne <etienne@email.com>
2026-08-02 04:29:51 +0000

    feat: apply custom task permission

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 
 from .models import Task
 from .serializers import TaskSerializer
-
+from core.permissions.task_permissions import IsTaskProjectOwner
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
@@ -10,7 +10,8 @@ class TaskViewSet(viewsets.ModelViewSet):
     """
 
     serializer_class = TaskSerializer
-
+    permission_classes = [IsTaskProjectOwner]
+    
     def get_queryset(self):
         """
         Retourne uniquement les tâches
